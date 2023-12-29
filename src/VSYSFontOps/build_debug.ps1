3ab70e6688ad2e582c49d0cff729e15fab6bc5b0
fmotion1 <futuremotionrecovery@gmail.com>
2023-12-28 19:41:48 -0600

    Fixing bugged commit

--- src/VSYSFontOps/build_debug.ps1
+++
@@ -1,5 +0,0 @@
-Push-Location $PSScriptRoot -StackName DotnetBuild
-dotnet build .\VSYSFontOps.csproj --configuration Debug
-Pop-Location -StackName DotnetBuild
-
-
--- src/VSYSFontOps/build_release.ps1
+++
@@ -1,6 +0,0 @@
-Push-Location $PSScriptRoot -StackName DotnetBuild
-dotnet build .\VSYSFontOps.csproj --configuration Release
-Pop-Location -StackName DotnetBuild
-Read-Host "Press any key to continue"
-
-
