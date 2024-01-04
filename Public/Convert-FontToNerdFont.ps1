71ffaee50d375354e0195d90430681967cf3c68c
fmotion1 <futuremotionrecovery@gmail.com>
2024-01-04 09:38:09 -0600

    Moved font-related scripts

---
+++ Public/Convert-FontToNerdFont.ps1
@@ -0,0 +1,16 @@
+﻿function Convert-FontToNerdFont {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]
+        $File
+    )
+
+    begin {
+        try {
+            Get-Command "$PSScriptRoot\..\bin\FontForge\bin\fontforge.exe"
+        }
+        catch {
+            Write-Error "Can't find bundled fontforge."
+            return
+        }
+    }
+}
